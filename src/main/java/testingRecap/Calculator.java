a0d50d15093a4b26f4a80ae4ebe34dfbf3c1d45f
Valerij Cerniuk <ptrank@gmail.com>
2021-12-05 11:46:00 +0200

    Added logger and some method in Calculator

--- src/main/java/testingRecap/Calculator.java
+++ src/main/java/testingRecap/Calculator.java
@@ -1,4 +1,22 @@
 package testingRecap;
 
+import org.apache.log4j.Logger;
+
 public class Calculator {
+
+    public static final Logger LOGGER = Logger.getLogger(Calculator.class);
+
+    public int add(int firstNumber, int seconNumbeer){
+        LOGGER.info("Adding two numbers " + firstNumber + " + " + seconNumbeer);
+        return firstNumber + seconNumbeer;
+    }
+
+    public int  addMultipleNumbers(int[] numbers){
+        int result = 0;
+        for(int number : numbers){
+            result += number;
+        }
+        return result;
+    }
+
 }
---
+++ src/main/java/testingRecap/Main.java
@@ -0,0 +1,10 @@
+package testingRecap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(12,5));
+
+    }
+}
